f3e037072af3aed26c7145abe66f1aab5329c4a1
ThanhBinh <binhthanh.vubc269@gmail.com>
2024-03-07 09:51:50 +0700

    thiet ke layout

---
+++ app/src/main/java/com/example/dhbc/layout_home1.java
@@ -0,0 +1,104 @@
+package com.example.dhbc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.io.IOException;
+
+public class layout_home1 extends AppCompatActivity {
+
+    ImageView as1,as2,bxh,avt,name,play1;
+    LinearLayout lin3;
+    MediaPlayer mp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_home1);
+        as1=findViewById(R.id.asleft);
+        as2=findViewById(R.id.asright);
+        bxh=findViewById(R.id.bxh);
+        avt=findViewById(R.id.lich);
+        name=findViewById(R.id.namegame);
+        lin3=findViewById(R.id.line3);
+        play1=findViewById(R.id.startgame);
+        Animation laclu1 = AnimationUtils.loadAnimation(this, R.anim.laclu);
+
+        Animation laclu2 = AnimationUtils.loadAnimation(this, R.anim.laclubtn);
+        Animation laclu3 = AnimationUtils.loadAnimation(this, R.anim.laclu3);
+        as1.startAnimation(laclu1);
+        name.startAnimation(laclu2);
+        as2.startAnimation(laclu3);
+        mp = new MediaPlayer();
+        try {
+            mp.setDataSource(getResources().openRawResourceFd(R.raw.chaomung0));
+            mp.prepare();
+            mp.start();
+            mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    // Hiển thị nút sau khi audio kết thúc
+                    play1.setVisibility(View.VISIBLE);
+                    lin3.setVisibility(View.VISIBLE);
+                }
+            });
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        play1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                play1.startAnimation(laclu2);
+                try {
+                    mp.reset();
+                    mp.setDataSource(getResources().openRawResourceFd(R.raw.huonglen1));
+                    mp.prepare();
+
+                    mp.start();
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (mp.isPlaying()) {
+                                mp.stop();
+                                startActivity(new Intent(layout_home1.this, layout_home2.class));
+                            }
+                        }
+                    }, 1000);
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Kiểm tra xem audio có được tạm dừng không và nếu có thì tiếp tục phát
+        if (mp != null && !mp.isPlaying()) {
+            mp.start();
+        }
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Kiểm tra nếu audio đang phát
+        if (mp != null && mp.isPlaying()) {
+            // Tạm dừng audio
+            mp.pause();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/dhbc/layout_home2.java
@@ -0,0 +1,123 @@
+package com.example.dhbc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Resources;
+import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+public class layout_home2 extends AppCompatActivity {
+
+    ImageView back, start,bacnoi;
+    TextView level,slgRuby,battat;
+    CheckBox nhacnen;
+    LinearLayout lin1;
+    static boolean nhacback=true;
+    MediaPlayer mp;
+    AnimationDrawable animationDrawable;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_home2);
+        back=findViewById(R.id.back1);
+        start=findViewById(R.id.startgame);
+        bacnoi=findViewById(R.id.bacnoi);
+        level=findViewById(R.id.level);
+        slgRuby=findViewById(R.id.ruby);
+        nhacnen=findViewById(R.id.nhacnen);
+        lin1=findViewById(R.id.line1);
+        battat=findViewById(R.id.battat);
+        nhacnen.setChecked(true);
+        back.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                layout_home2.this.finish();
+            }
+        });
+        nhacnen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CheckBox checkBox = (CheckBox) v;
+                if (checkBox.isChecked()) {
+                   nhacback= !nhacback;
+                   battat.setText("Nhạc nền đã bật");
+
+                } else {
+                    nhacback= !nhacback;
+
+                    battat.setText("Nhạc nền đã tắt");
+                }
+                lin1.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+        Resources res = getResources();
+        AnimationDrawable animationDrawable = (AnimationDrawable) res.getDrawable(R.drawable.listanh);
+        bacnoi.setImageDrawable(animationDrawable);
+        mp = new MediaPlayer();
+        // Bắt đầu animation
+        animationDrawable.start();
+        try {
+            mp.setDataSource(getResources().openRawResourceFd(R.raw.rule0));
+            mp.prepare();
+            mp.start();
+//            mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+//                @Override
+//                public void onCompletion(MediaPlayer mediaPlayer) {
+//                    // Hiển thị nút sau khi audio kết thúc
+//                    play1.setVisibility(View.VISIBLE);
+//                    lin3.setVisibility(View.VISIBLE);
+//                }
+//            });
+            mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    animationDrawable.stop();
+                }
+            });
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Giải phóng MediaPlayer khi activity bị destroy
+        if (mp != null) {
+            mp.release();
+            mp = null;
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Kiểm tra xem audio có được tạm dừng không và nếu có thì tiếp tục phát
+        if (mp != null && !mp.isPlaying()) {
+            mp.start();
+        }
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Kiểm tra nếu audio đang phát
+        if (mp != null && mp.isPlaying()) {
+            // Tạm dừng audio
+            mp.pause();
+        }
+    }
+}
